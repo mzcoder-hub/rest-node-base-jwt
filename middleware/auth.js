3dfd3665bff8971443dbad96137fb95c9370e15a
Galang Yoga Pradana <galangypradana@gmail.com>
2019-12-27 12:40:21 +0700

    reset Password with email using sendgrid, update middleware check from url

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,20 +3,29 @@ const config = require('config');
 
 module.exports = function(req, res, next) {
 	//get Token from header
-	const token = req.header('x-auth-token');
+	const tokenHeader = req.header('x-auth-token');
+	const tokenUri = req.params.token;
+	if (!tokenHeader && tokenUri) {
+		//verify token
+		try {
+			const decode = jwt.verify(tokenUri, config.get('jwtSecret'));
 
-	//check if not token
-	if (!token) {
+			req.user = decode.user;
+			next();
+		} catch (err) {
+			res.status(401).json({ msg: 'message is not valid' });
+		}
+	} else if (tokenHeader && !tokenUri) {
+		//verify token
+		try {
+			const decode = jwt.verify(tokenHeader, config.get('jwtSecret'));
+
+			req.user = decode.user;
+			next();
+		} catch (err) {
+			res.status(401).json({ msg: 'message is not valid' });
+		}
+	} else {
 		return res.status(401).json({ msg: 'No Token, Authorization Denied' });
-	}
-
-	//verify token
-	try {
-		const decode = jwt.verify(token, config.get('jwtSecret'));
-
-		req.user = decode.user;
-		next();
-	} catch (err) {
-		res.status(401).json({ msg: 'message is not valid' });
 	}
 };
--- routes/api/Auth.js
+++ routes/api/Auth.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const config = require('config');
+const sgMail = require('@sendgrid/mail');
 const { check, validationResult } = require('express-validator');
 
 require('../../passport')();
@@ -221,4 +222,380 @@ router.get('/google/accessToken', (req, res) => {
 	);
 });
 
+// @route    GET api/Auth/resetPassword
+// @desc     Reset Password Only for Email
+// @access   Public
+
+router.post(
+	'/resetPassword',
+	[check('email', 'Please Include a Valid email').isEmail()],
+	async (req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email } = req.body;
+		try {
+			// see if user exist
+			let user = await User.findOne({ email: email });
+
+			//see if user not founded
+			if (!user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid Credentials' }] });
+			}
+
+			//return jsonwebtekon
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					const generateJwt = token;
+					sgMail.setApiKey(config.get('sendGrid_API_KEY'));
+					const msg = {
+						to: `${email}`,
+						from: 'noreply@snakers.com',
+						subject: 'Reset Password - Snackers',
+						text: 'Your Reset Password Link Click Here :',
+						html: `
+						<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml"><head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+      <!--[if !mso]><!-->
+      <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+      <!--<![endif]-->
+      <!--[if (gte mso 9)|(IE)]>
+      <xml>
+        <o:OfficeDocumentSettings>
+          <o:AllowPNG/>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+      </xml>
+      <![endif]-->
+      <!--[if (gte mso 9)|(IE)]>
+  <style type="text/css">
+    body {width: 600px;margin: 0 auto;}
+    table {border-collapse: collapse;}
+    table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
+    img {-ms-interpolation-mode: bicubic;}
+  </style>
+<![endif]-->
+      <style type="text/css">
+    body, p, div {
+      font-family: arial,helvetica,sans-serif;
+      font-size: 14px;
+    }
+    body {
+      color: #000000;
+    }
+    body a {
+      color: #1188E6;
+      text-decoration: none;
+    }
+    p { margin: 0; padding: 0; }
+    table.wrapper {
+      width:100% !important;
+      table-layout: fixed;
+      -webkit-font-smoothing: antialiased;
+      -webkit-text-size-adjust: 100%;
+      -moz-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+    }
+    img.max-width {
+      max-width: 100% !important;
+    }
+    .column.of-2 {
+      width: 50%;
+    }
+    .column.of-3 {
+      width: 33.333%;
+    }
+    .column.of-4 {
+      width: 25%;
+    }
+    @media screen and (max-width:480px) {
+      .preheader .rightColumnContent,
+      .footer .rightColumnContent {
+        text-align: left !important;
+      }
+      .preheader .rightColumnContent div,
+      .preheader .rightColumnContent span,
+      .footer .rightColumnContent div,
+      .footer .rightColumnContent span {
+        text-align: left !important;
+      }
+      .preheader .rightColumnContent,
+      .preheader .leftColumnContent {
+        font-size: 80% !important;
+        padding: 5px 0;
+      }
+      table.wrapper-mobile {
+        width: 100% !important;
+        table-layout: fixed;
+      }
+      img.max-width {
+        height: auto !important;
+        max-width: 100% !important;
+      }
+      a.bulletproof-button {
+        display: block !important;
+        width: auto !important;
+        font-size: 80%;
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+      }
+      .columns {
+        width: 100% !important;
+      }
+      .column {
+        display: block !important;
+        width: 100% !important;
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+        margin-left: 0 !important;
+        margin-right: 0 !important;
+      }
+    }
+  </style>
+      <!--user entered Head Start--><!--End Head user entered-->
+    </head>
+    <body>
+      <center class="wrapper" data-link-color="#1188E6" data-body-style="font-size:14px; font-family:arial,helvetica,sans-serif; color:#000000; background-color:#FFFFFF;">
+        <div class="webkit">
+          <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#FFFFFF">
+            <tbody><tr>
+              <td valign="top" bgcolor="#FFFFFF" width="100%">
+                <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
+                  <tbody><tr>
+                    <td width="100%">
+                      <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                        <tbody><tr>
+                          <td>
+                            <!--[if mso]>
+    <center>
+    <table><tr><td width="600">
+  <![endif]-->
+                                    <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%; max-width:600px;" align="center">
+                                      <tbody><tr>
+                                        <td role="modules-container" style="padding:0px 0px 0px 0px; color:#000000; text-align:left;" bgcolor="#FFFFFF" width="100%" align="left"><table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%" style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
+    <tbody><tr>
+      <td role="module-content">
+        <p></p>
+      </td>
+    </tr>
+  </tbody></table><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" role="module" data-type="columns" style="padding:0px 0px 0px 0px;" bgcolor="#FFFFFF">
+    <tbody>
+      <tr role="module-content">
+        <td height="100%" valign="top">
+          <table class="column" width="580" style="width:580px; border-spacing:0; border-collapse:collapse; margin:0px 10px 0px 10px;" cellpadding="0" cellspacing="0" align="left" border="0" bgcolor="">
+            <tbody>
+              <tr>
+                <td style="padding:0px;margin:0px;border-spacing:0;"><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="b11fddbf-a5b3-420f-b0eb-179ba304e19a">
+    <tbody>
+      <tr>
+        <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+          <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px;" width="80" alt="" data-proportionally-constrained="true" data-responsive="false" src="http://cdn.mcauto-images-production.sendgrid.net/eea1bd788ce6038b/c9482dc0-7609-478d-ae37-2a9357339458/640x640.jpg" height="80">
+        </td>
+      </tr>
+    </tbody>
+  </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="19581b7b-d377-44e8-8352-88b213aa9031" data-mc-module-version="2019-10-22">
+    <tbody>
+      <tr>
+        <td style="padding:18px 0px 18px 0px; line-height:0px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><h2 style="text-align: center">Snackers - Voucher and Coupon discount</h2><div></div></div></td>
+      </tr>
+    </tbody>
+  </table><table class="module" role="module" data-type="divider" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="76696bc4-19c2-4b34-88bb-046601e17b54">
+    <tbody>
+      <tr>
+        <td style="padding:0px 0px 0px 0px;" role="module-content" height="100%" valign="top" bgcolor="">
+          <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" height="10px" style="line-height:10px; font-size:10px;">
+            <tbody>
+              <tr>
+                <td style="padding:0px 0px 10px 0px;" bgcolor="#000000"></td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="539bbb9f-5330-4b8b-8129-e4b4d1f35107" data-mc-module-version="2019-10-22">
+    <tbody>
+      <tr>
+        <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit"><span style="font-family: &quot;lucida sans unicode&quot;, &quot;lucida grande&quot;, sans-serif">This is Your Reset Password:</span></div><div></div></div></td>
+      </tr>
+    </tbody>
+  </table><table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="f0a7ff00-25d5-42f4-acad-745d519d9b72">
+    <tbody>
+      <tr>
+        <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="">
+        </td>
+      </tr>
+    </tbody>
+  </table><table border="0" cellpadding="0" cellspacing="0" class="module" data-role="module-button" data-type="button" role="module" style="table-layout:fixed;" width="100%" data-muid="d9166861-1275-49e4-908a-4bbbd9dda4d9">
+      <tbody>
+        <tr>
+          <td align="center" bgcolor="" class="outer-td" style="padding:0px 0px 0px 0px;">
+            <table border="0" cellpadding="0" cellspacing="0" class="wrapper-mobile" style="text-align:center;">
+              <tbody>
+                <tr>
+                <td align="center" bgcolor="#333333" class="inner-td" style="border-radius:6px; font-size:16px; text-align:center; background-color:inherit;">
+                  <a href="${config.get(
+										'UrlReset'
+									)}${generateJwt}" style="background-color:#333333; border:1px solid #333333; border-color:#333333; border-radius:6px; border-width:1px; color:#ffffff; display:inline-block; font-size:14px; font-weight:normal; letter-spacing:0px; line-height:normal; padding:12px 18px 12px 18px; text-align:center; text-decoration:none; border-style:solid;" target="_blank">Reset Password</a>
+                </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table><table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="bff20f0e-c365-44b1-8b41-9d91c6699f12">
+    <tbody>
+      <tr>
+        <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="">
+        </td>
+      </tr>
+    </tbody>
+  </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="f8eeb304-0071-4193-b147-66b7807483a2" data-mc-module-version="2019-10-22">
+    <tbody>
+      <tr>
+        <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit">If that button doesn't work use this link :</div><div></div></div></td>
+      </tr>
+    </tbody>
+  </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="ede4cb2f-fc72-4e43-8f93-773ea4da5777" data-mc-module-version="2019-10-22">
+    <tbody>
+      <tr>
+        <td style="padding:18px 0px 18px 0px; line-height:30px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit"><a href="${config.get(
+					'UrlReset'
+				)}${generateJwt}"><strong>${config.get(
+							'UrlReset'
+						)}${generateJwt}</strong></a></div><div></div></div></td>
+      </tr>
+    </tbody>
+  </table><table class="module" role="module" data-type="divider" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="fab6a375-ca58-4e8c-b447-499fe08cdd5a">
+    <tbody>
+      <tr>
+        <td style="padding:0px 0px 0px 0px;" role="module-content" height="100%" valign="top" bgcolor="">
+          <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" height="10px" style="line-height:10px; font-size:10px;">
+            <tbody>
+              <tr>
+                <td style="padding:0px 0px 10px 0px;" bgcolor="#000000"></td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table></td>
+              </tr>
+            </tbody>
+          </table>
+          
+        </td>
+      </tr>
+    </tbody>
+  </table><div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5">
+                                            <div class="Unsubscribe--addressLine"></div>
+                                            <p style="font-size:12px; line-height:20px;"><a class="Unsubscribe--unsubscribeLink" href="{{{unsubscribe}}}" target="_blank" style="">Unsubscribe</a></p>
+                                          </div></td>
+                                      </tr>
+                                    </tbody></table>
+                                    <!--[if mso]>
+                                  </td>
+                                </tr>
+                              </table>
+                            </center>
+                            <![endif]-->
+                          </td>
+                        </tr>
+                      </tbody></table>
+                    </td>
+                  </tr>
+                </tbody></table>
+              </td>
+            </tr>
+          </tbody></table>
+        </div>
+      </center>
+    
+  
+</body></html>
+						`
+					};
+					const send = sgMail.send(msg);
+
+					if (send) {
+						return res.json({ msg: 'Reset Password Link Has been sended' });
+					}
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
+
+// @route    POST api/auth/email/resetPassword/:token
+// @desc     Login User  Using Email
+// @access   Public
+
+router.post(
+	'/email/:token',
+	auth,
+	[
+		check('password', 'Password is required').exists(),
+		check('password2', 'Confirmation Password is required').exists()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { password, password2 } = req.body;
+
+		try {
+			if (password2 !== password) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Password Tidak Sama' }] });
+			} else {
+				// see if user exist
+				let user = await User.findById(req.user.id).select('-password');
+
+				// console.log(user);
+				//see if user not founded
+				if (!user) {
+					return res
+						.status(400)
+						.json({ errors: [{ msg: 'Invalid Credentials' }] });
+				} else {
+					const salt = await bcrypt.genSalt(10);
+
+					const newPassword = await bcrypt.hash(password, salt);
+
+					User.findOneAndUpdate({ _id: req.user.id }, { password: newPassword })
+						.then(() => res.status(202).json('Password changed accepted'))
+						.catch(err => res.status(500).json(err));
+				}
+			}
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 module.exports = router;
